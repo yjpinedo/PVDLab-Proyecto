efdeb178eda0ab882913f08a670960d936bf046b
kelly <k.vargas.c31@hotmail.com>
2019-07-07 10:32:32 -0500

    Update config

--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,53 @@ Auth::routes(['verify' => true]);
 Route::redirect('/', 'home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+
     Route::get('home', 'HomeController@index')->name('home');
 
+    // Admin
+    Route::middleware(['role:admin'])->group(function () {
 
-    Route::resource('teachers', 'TeacherController', ['except' => ['create', 'edit']]);
-    Route::resource('beneficiaries', 'BeneficiaryController', ['except' => ['create', 'edit']]);
-    Route::resource('courses', 'CourseController', ['except' => ['create', 'edit']]);
-    Route::resource('lessons', 'LessonController', ['except' => ['create', 'edit']]);
-    Route::resource('beneficiary_courses', 'BeneficiaryCourseController', ['except' => ['create', 'edit']]);
-    Route::resource('beneficiary_lessons', 'BeneficiaryLessonController', ['except' => ['create', 'edit']]);
-    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-    Route::resource('locations', 'LocationController', ['except' => ['create', 'edit']]);
-    Route::resource('furniture', 'FurnitureController', ['except' => ['create', 'edit']]);
-    Route::resource('positions', 'PositionController', ['except' => ['create', 'edit']]);
-    Route::resource('employees', 'EmployeeController', ['except' => ['create', 'edit']]);
-    Route::resource('projects', 'ProjectController', ['except' => ['create', 'edit']]);
-    Route::resource('beneficiary_projects', 'BeneficiaryProjectController', ['except' => ['create', 'edit']]);
-    Route::resource('transfers', 'TransferController', ['except' => ['create', 'edit']]);
-    Route::resource('furniture_transfers', 'FurnitureTransferController', ['except' => ['create', 'edit']]);
+        // Teachers
+        Route::resource('teachers', 'TeacherController', ['except' => ['create', 'edit']]);
+        // Beneficiaries
+        Route::resource('beneficiaries', 'BeneficiaryController', ['except' => ['create', 'edit']]);
+        // Courses
+        Route::resource('courses', 'CourseController', ['except' => ['create', 'edit']]);
+        // Lessons
+        Route::resource('lessons', 'LessonController', ['except' => ['create', 'edit']]);
+        // Beneficiaries - Courses
+        Route::resource('beneficiary_courses', 'BeneficiaryCourseController', ['except' => ['create', 'edit']]);
+        // Beneficiaries - Lessons
+        Route::resource('beneficiary_lessons', 'BeneficiaryLessonController', ['except' => ['create', 'edit']]);
+        // Categories
+        Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+        // Locations
+        Route::resource('locations', 'LocationController', ['except' => ['create', 'edit']]);
+        // Furniture
+        Route::resource('furniture', 'FurnitureController', ['except' => ['create', 'edit']]);
+        // Positions
+        Route::resource('positions', 'PositionController', ['except' => ['create', 'edit']]);
+        // Employees
+        Route::resource('employees', 'EmployeeController', ['except' => ['create', 'edit']]);
+        // Projects
+        Route::resource('projects', 'ProjectController', ['except' => ['create', 'edit']]);
+        // Projects - Beneficiaries
+        Route::resource('beneficiary_projects', 'BeneficiaryProjectController', ['except' => ['create', 'edit']]);
+        Route::resource('transfers', 'TransferController', ['except' => ['create', 'edit']]);
+        // Transfers
+        Route::resource('furniture_transfers', 'FurnitureTransferController', ['except' => ['create', 'edit']]);
+        // Transfer - Furniture
+
+    });
+
+    // Beneficiaries
+    Route::middleware(['role:beneficiaries'])->namespace('Beneficiary')->prefix('beneficiary')->group(function () {
+        // Courses
+        Route::resource('courses', 'CourseController', ['except' => ['create', 'destroy', 'edit', 'store']])->names('beneficiary.courses');
+        // Projects
+        Route::resource('projects', 'ProjectController', ['except' => ['create', 'destroy']])->names('beneficiary.projects');
+
+    });
 
     Route::get('select', function (Request $request) {
         $request->request->add(['data' => Base::select($request->input('name'))]);

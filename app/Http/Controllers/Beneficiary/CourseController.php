9ec2c37a3b0ed2e963cef38f286d8b953d64cdf2
kelly <k.vargas.c31@hotmail.com>
2019-07-07 11:03:54 -0500

    Create Controller Role - Beneficiary

---
+++ app/Http/Controllers/Beneficiary/CourseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Beneficiary;
+
+use App\Beneficiary;
+use App\Course;
+use App\Http\Controllers\BaseController;
+use Illuminate\Support\Facades\Auth;
+
+class CourseController extends BaseController
+{
+    /**
+     * Create a controller instance.
+     *
+     * @param Course $entity
+     */
+    public function __construct(Course $entity)
+    {
+        parent::__construct($entity);
+
+        $this->crud = 'beneficiary.courses';
+
+        $this->middleware(function ($request, $next) {
+            $beneficiary = Beneficiary::where('id', Auth::user()['model_id'])->first();
+
+            if ( !is_null($beneficiary) ) {
+                $request->request->add(['data' => [
+                    'tools' => [
+                        'create' => false,
+                        'reload' => false,
+                        'export' => false,
+                    ],
+                    'table' => [
+                        'check' => false,
+                        'fields' => ['id', 'code', 'name'],
+                        'active' => false,
+                        'actions' => false,
+                    ],
+                ]]);
+
+                $request->request->add(['beneficiary_id' => $beneficiary->id]);
+                $this->model = $beneficiary->courses->sortByDesc('name');
+
+                return $next($request);
+            }
+
+            return abort(404);
+        });
+    }
+}
---
+++ app/Http/Controllers/Beneficiary/ProjectController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers\Beneficiary;
+
+use App\Beneficiary;
+use App\Http\Controllers\BaseController;
+use App\Project;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectController extends BaseController
+{
+
+    /**
+     * Create a controller instance.
+     *
+     * @param Project $entity
+     */
+    public function __construct(Project $entity)
+    {
+        parent::__construct($entity);
+
+        $this->crud = 'beneficiary.projects';
+
+        $this->middleware(function ($request, $next) {
+            $beneficiary = Beneficiary::where('id', Auth::user()['model_id'])->first();
+            if ( !is_null($beneficiary) ) {
+                $request->request->add(['data' => [
+                    'tools' => [
+                        'create' => false,
+                        'reload' => false,
+                        'export' => false,
+                    ],
+                    'table' => [
+                        'check' => false,
+                        'fields' => ['code', 'name', 'start', 'type', 'concept'],
+                        'active' => false,
+                        'actions' => false,
+                    ],
+                    'form' => [
+                        [
+                            'type' => 'section',
+                            'value' => 'app.sections.project_information',
+                        ],
+                        [
+                            'name' => 'code',
+                            'type' => 'text',
+                        ],
+                        [
+                            'name' => 'name',
+                            'type' => 'text',
+                        ],
+                        [
+                            'name' => 'date',
+                            'type' => 'date',
+                        ],
+                        [
+                            'name' => 'start',
+                            'type' => 'date',
+                        ],
+                        [
+                            'name' => 'type',
+                            'type' => 'select',
+                            'value' => 'app.selects.project.type',
+                        ],
+                        [
+                            'name' => 'other_type',
+                            'type' => 'text',
+                        ],
+                        [
+                            'name' => 'description',
+                            'type' => 'textarea',
+                        ],
+                        [
+                            'name' => 'origin',
+                            'type' => 'select',
+                            'value' => 'app.selects.project.origin',
+                        ],
+                        [
+                            'name' => 'other_origin',
+                            'type' => 'text',
+                        ],
+                        [
+                            'name' => 'state',
+                            'type' => 'select',
+                            'value' => 'app.selects.project.state',
+                        ],
+                        [
+                            'type' => 'section',
+                            'value' => 'app.sections.financing_information',
+                        ],
+                        [
+                            'name' => 'financing',
+                            'type' => 'select',
+                            'value' => 'app.selects.project.financing',
+                        ],
+                        [
+                            'name' => 'financial_entity',
+                            'type' => 'text',
+                        ],
+                        [
+                            'name' => 'financing_description',
+                            'type' => 'textarea',
+                        ],
+                    ],
+                ]]);
+                $request->request->add(['beneficiary_id' => $beneficiary->id]);
+                $this->model = $beneficiary->projects->sortByDesc('name');
+
+                return $next($request);
+            }
+
+            return abort(404);
+        });
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function statusUpdate(Request $request)
+    {
+        $turn = $this->entity::find($request->input('id'));
+
+        if ( is_null($turn) ) return abort(404);
+
+        if ($request->input('state') == __('app.selects.turns.state_next.' . $turn->state)) {
+            $turn->state = $request->input('state');
+            $turn->save();
+        }
+
+        return response()->json([
+            'message' => __('app.messages.turns.' . $turn->state),
+        ]);
+    }
+}

d892b5706983b25488b31098c4ae0d9b5dcb005c
kelly <k.vargas.c31@hotmail.com>
2019-07-07 10:07:43 -0500

    Add Attribute model_id, model_type

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'model_id', 'model_type',
     ];
 
     /**

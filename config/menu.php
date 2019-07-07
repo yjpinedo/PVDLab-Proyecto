ee74d54f73c43d92dc462a747cab485e15b48057
kelly <k.vargas.c31@hotmail.com>
2019-07-07 09:59:00 -0500

    Update menu

--- config/menu.php
+++ config/menu.php
@@ -3,10 +3,24 @@
 return [
 
     [
+        'name' => 'beneficiaries',
+        'menu' => [
+            [
+                'crud' => 'beneficiary.courses',
+                'icon' => 'fa fa-users',
+            ],
+            [
+                'crud' => 'beneficiary.projects',
+                'icon' => 'fa fa-users',
+            ],
+        ],
+    ],
+
+    [
         'name' => 'admin',
         'menu' => [
             [
-                'crud' => 'employees',
+                'crud' => 'beneficiaries',
                 'icon' => 'fa fa-users',
             ],
         ],

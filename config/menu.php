972b6b68398c92d26159386631c550fb4ae2a6c7
kelly <k.vargas.c31@hotmail.com>
2019-07-08 17:22:49 -0500

    Configure Menu and add Beneficiary - Lesson

--- config/menu.php
+++ config/menu.php
@@ -3,15 +3,33 @@
 return [
 
     [
-        'name' => 'beneficiaries',
+        'name' => 'beneficiary',
         'menu' => [
             [
-                'crud' => 'beneficiary.courses',
-                'icon' => 'fa fa-users',
+                'crud' => 'beneficiary.projects',
+                'icon' => 'fa fa-project-diagram',
             ],
             [
-                'crud' => 'beneficiary.projects',
-                'icon' => 'fa fa-users',
+                'crud' => 'beneficiary.courses',
+                'icon' => 'fa fa-clipboard-list',
+            ],
+            [
+                'crud' => 'beneficiary.lessons',
+                'icon' => 'fa fa-list-ul',
+            ],
+        ],
+    ],
+
+    [
+        'name' => 'teacher',
+        'menu' => [
+            [
+                'crud' => 'teacher.courses',
+                'icon' => 'fa fa-clipboard-list',
+            ],
+            [
+                'crud' => 'teacher.lessons',
+                'icon' => 'fa fa-list-ul',
             ],
         ],
     ],
@@ -20,8 +38,54 @@ return [
         'name' => 'admin',
         'menu' => [
             [
+                'crud' => 'projects',
+                'icon' => 'fa fa-project-diagram',
+            ],
+            [
                 'crud' => 'beneficiaries',
                 'icon' => 'fa fa-users',
+            ],
+            [
+                'icon' => 'fa fa-boxes',
+                'name' => 'furniture',
+                'submenu' => [
+                    [
+                        'crud' => 'transfers',
+                    ],
+                    [
+                        'crud' => 'furniture',
+                    ],
+                    [
+                        'crud' => 'locations',
+                    ],
+                    [
+                        'crud' => 'categories',
+                    ],
+                ],
+            ],
+            [
+                'icon' => 'fa fa-clipboard-list',
+                'name' => 'courses',
+                'submenu' => [
+                    [
+                        'crud' => 'courses',
+                    ],
+                    [
+                        'crud' => 'teachers',
+                    ],
+                ],
+            ],
+            [
+                'icon' => 'fa fa-user-tie',
+                'name' => 'employees',
+                'submenu' => [
+                    [
+                        'crud' => 'employees',
+                    ],
+                    [
+                        'crud' => 'positions',
+                    ],
+                ],
             ],
         ],
     ],
